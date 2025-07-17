0cbe7a43dc5b47fa62c7d06952b5fd4179eb192e
zlc1004 <lucaszhang1001@gmail.com>
2025-07-18 05:05:21 +0800

    send verr email meathof

--- app/imports/api/accounts/AccountsMethods.js
+++ app/imports/api/accounts/AccountsMethods.js
@@ -1,4 +1,13 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 
 Meteor.methods({
-  async 'accounts.email.send.verification'() {} });
+  async 'accounts.email.send.verification'() {
+    const user = await Accounts.userAsync();
+    if (!user) {
+      return false;
+    }
+    const result = await Accounts.sendVerificationEmail(user._id);
+    return result;
+  },
+});
