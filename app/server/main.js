8beca6d50895813383542e2aa9bff933417ddd01
zlc1004 <lucaszhang1001@gmail.com>
2025-07-18 04:10:32 +0800

    relocate captcha

--- app/server/main.js
+++ app/server/main.js
@@ -2,7 +2,7 @@ import "/imports/startup/server/Accounts";
 import "/imports/startup/server/Publications";
 import "/imports/startup/server/Mongo";
 import "/imports/startup/server/AdminMethods";
-import "/imports/startup/server/CaptchaMethods";
+import "/imports/api/captcha/CaptchaMethods";
 import "/imports/api/accounts/AccountsMethods"
 import "/imports/api/images/ImageMethods";
 import "/imports/api/chat/ChatMethods";
