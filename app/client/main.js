d54fa1c80203133622ffbcaefc350605ec6e7256
zlc1004 <lucaszhang1001@gmail.com>
2025-08-04 00:11:41 +0800

    style(app/client): reorder and clean up imports - Reordered imports to ensure consistent ordering - Removed duplicate console.log statements

--- app/client/main.js
+++ app/client/main.js
@@ -1,6 +1,2 @@
-console.log('CLIENT MAIN.JS EXECUTING!!!');
-
-import '../imports/startup/client/Startup';
-import './style.css';
-
-console.log('CLIENT MAIN.JS IMPORTS COMPLETED!!!');
+import "../imports/startup/client/Startup";
+import "./style.css";
