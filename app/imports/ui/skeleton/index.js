bc92ceea0433c40402a9988f92af0ceb6ea5793a
zlc1004 <lucaszhang1001@gmail.com>
2025-08-15 01:52:36 +0800

    refactor(skeleton): remove style exports from index
    - Remove styled-components exports from skeleton index
    - Keep only component exports for cleaner imports
    - Styles are internal implementation details

--- app/imports/ui/skeleton/index.js
+++ app/imports/ui/skeleton/index.js
@@ -1,7 +1,3 @@
 // Skeleton components exports
 export { default as MyRidesSkeleton } from "./pagesComponent/MyRidesSkeleton";
 export { default as ChatSkeleton } from "./pagesComponent/ChatSkeleton";
-
-// Skeleton styles exports (if needed)
-export * from "./styles/MyRidesSkeleton";
-export * from "./styles/ChatSkeleton";
